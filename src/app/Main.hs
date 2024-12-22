686f6e5c974f4c4761b92e5472886dd25327bc0e
jtamu <gtamu.dip@gmail.com>
2024-12-23 01:41:48 +0900

    feat: WAI & Warpでhello world

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,4 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Network.HTTP.Types (status200)
+import Network.Wai (Application, responseLBS)
+import Network.Wai.Handler.Warp (run)
+
+app :: Application
+app _ respond = do
+  putStrLn "I've done some IO here"
+  respond $
+    responseLBS
+      status200
+      [("Content-Type", "text/plain")]
+      "Hello, World!"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Starting server at http://localhost:8080 ..."
+  run 8080 app
