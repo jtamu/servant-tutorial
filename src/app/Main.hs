77bf1448b389eca58cf104c454a71e7c9cecd7eb
jtamu <gtamu.dip@gmail.com>
2024-12-23 01:53:22 +0900

    feat: 標準出力のバッファリングを無効化

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import Network.HTTP.Types (status200)
 import Network.Wai (Application, responseLBS)
 import Network.Wai.Handler.Warp (run)
+import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
 
 app :: Application
 app _ respond = do
@@ -17,5 +18,6 @@ app _ respond = do
 
 main :: IO ()
 main = do
+  hSetBuffering stdout NoBuffering
   putStrLn "Starting server at http://localhost:8080 ..."
   run 8080 app
