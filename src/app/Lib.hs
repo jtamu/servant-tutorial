deeee4425db5d3a04f770d19042683ea305ba317
jtamu <gtamu.dip@gmail.com>
2024-12-29 02:08:34 +0900

    first servant

---
+++ src/app/Lib.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Lib where
+
+import Data.Aeson (ToJSON)
+import Data.Time.Calendar (Day, fromGregorian)
+import GHC.Generics (Generic)
+import Servant ((:>), Get, JSON, Server, Proxy(Proxy), serve)
+import Network.Wai(Application)
+import Network.Wai.Handler.Warp(run)
+
+data User = User
+  { name :: String,
+    age :: Int,
+    email :: String,
+    registration_date :: Day
+  }
+  deriving (Eq, Show, Generic)
+
+instance ToJSON User
+
+users :: [User]
+users = [
+  User { name = "Alice", age = 25, email = "alice@example.com", registration_date = (fromGregorian 2020 1 1) },
+  User { name = "Bob", age = 30, email = "bob@example.com", registration_date = (fromGregorian 2021 1 1) }
+  ]
+
+type UserAPI1 = "users" :> Get '[JSON] [User]
+
+server :: Server UserAPI1
+server = return users
+
+userAPI :: Proxy UserAPI1
+userAPI = Proxy
+
+app1 :: Application
+app1 = serve userAPI server
+
+runServer :: IO ()
+runServer = run 8080 app1
--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,23 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
-import Network.HTTP.Types (status200)
-import Network.Wai (Application, responseLBS)
-import Network.Wai.Handler.Warp (run)
-import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
-
-app :: Application
-app _ respond = do
-  putStrLn "I've done some IO here"
-  respond $
-    responseLBS
-      status200
-      [("Content-Type", "text/plain")]
-      "Hello, World!"
+import Lib (runServer)
 
 main :: IO ()
-main = do
-  hSetBuffering stdout NoBuffering
-  putStrLn "Starting server at http://localhost:8080 ..."
-  run 8080 app
+main = runServer
