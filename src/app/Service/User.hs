b7bcaedca1df263b0eb2e15e5db6c7b38f32cf46
jtamu <gtamu.dip@gmail.com>
2025-01-28 00:56:10 +0900

    refactor: ユーザ更新のロジックを少しましな形にする

--- src/app/Service/User.hs
+++ src/app/Service/User.hs
@@ -7,7 +7,7 @@ import Control.Monad.State (execState)
 import Data.Int (Int64)
 import Data.Map qualified as M
 import Data.Maybe (fromMaybe)
-import Data.Validation (Validation (Failure, Success))
+import Data.Validation (Validation (Failure, Success), bindValidation)
 import Domain.User qualified as Domain
 import Dto.UserUpdate (UserUpdateDto (_age, _email, _name, _registrationDate))
 import Dto.ValidationError (ValidationError (ValidationError))
@@ -17,24 +17,23 @@ import Repository.User qualified as R (update)
 update :: UserRepository -> Int64 -> UserUpdateDto -> IO (Validation ValidationError ())
 update r uid dto = do
   maybeUser <- r.get uid
-  case maybeUser of
-    Nothing -> return $ Failure $ ValidationError $ M.fromList [("user", ["該当するユーザは存在しません"])]
-    (Just user) ->
-      mapM r.update (_update dto user)
+  let vuser = liftMaybe (ValidationError $ M.fromList [("user", ["該当するユーザは存在しません"])]) maybeUser
+  mapM r.update $ vuser `bindValidation` _update dto
 
 _update :: UserUpdateDto -> Domain.RegisteredUser -> Validation ValidationError Domain.RegisteredUser
 _update dto user =
-  let vAge = case dto._age of
-        (Just a) -> Domain.makeAge a
-        Nothing -> Success user._age
-   in case vAge of
-        (Success age) ->
-          Success $
-            ( execState $ do
-                modifying Domain.name (\n -> fromMaybe n dto._name)
-                modifying Domain.age (const age)
-                modifying Domain.email (\e -> fromMaybe e dto._email)
-                modifying Domain.registrationDate (\r -> fromMaybe r dto._registrationDate)
-            )
-              user
-        (Failure err) -> Failure err
+  updateUser <$> vAge
+  where
+    vAge = maybe (Success user._age) Domain.makeAge dto._age
+    updateUser age =
+      ( execState $ do
+          modifying Domain.name (\n -> fromMaybe n dto._name)
+          modifying Domain.age (const age)
+          modifying Domain.email (\e -> fromMaybe e dto._email)
+          modifying Domain.registrationDate (\r -> fromMaybe r dto._registrationDate)
+      )
+        user
+
+liftMaybe :: b -> Maybe a -> Validation b a
+liftMaybe b Nothing = Failure b
+liftMaybe _ (Just x) = Success x
